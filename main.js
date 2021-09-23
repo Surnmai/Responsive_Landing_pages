933888c3209e613dea2c762013dcf2faa9770a18
Surnmai <surnmai1@gmail.com>
2021-09-23 10:34:45 +0000

    init

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ bottles.forEach(function(item){
         // console.log(colorID.currentTarget.dataset.id);
         const id = colorID.currentTarget.dataset.id
         const bgColor = document.querySelector('.sec')
+        const mainPep = document.querySelector('.bigCann')
         if (id === '1') {
             bgColor.style.background = '#0062be'
             mainPep.src = './images/pepsi001.png'
